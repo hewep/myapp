3e580b3201a5473828b7d5b9feee0d40810e1e36
hewep <hewephao@163.com>
2014-06-18 22:56:04 +0800

    发表话题

--- WebRoot/res/media/controllers/index.js
+++ WebRoot/res/media/controllers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 /* Controllers */
 define( function(){
+	// 话题类别
 	var CategoryListCtrl = ['$scope', '$http', function($scope, $http){
 		$scope.categorys = {};
 		
@@ -13,25 +14,17 @@ define( function(){
 		});
 	}];
 	
+	//话题
 	var TopicCtrl = ['$scope', '$http', function($scope, $http){
 		$scope.topic = {};
 		$scope.categorys = {};
-		$scope.category = $scope.categorys[0];
-		
-		$http({	method:'post',
-				url:"category/list?type=tree"
-		}).success(function(result){
-			$scope.categorys = angular.fromJson(result.data);
-		}).error(function(){
-			alert("网络连接失败");
-		});
 		
 		$scope.changeCate = function(category){
 			$scope.category = category;
 		};
 		
 		$scope.save = function(){
-			$scope.topic.content = $scope.editor.html();
+			$scope.topic.content = $scope.editor.html();	// 通过kindEditor  指令引用
 			$scope.topic.category_id = $("#type a.selected").attr("type");
 			
 			$http({method:'post',url:"topic/addOrUpdate", params:$scope.topic}).success(function(result){
@@ -47,11 +40,23 @@ define( function(){
 		init();
 		
 		function init(){
+			
+			// 初始化 类型选项
 			$("#type").delegate("a","click",function(){
 				$(this).addClass("selected");
-				$(this).siblings().each(function(){
-					$(this).removeClass("selected");
+				$(this).closest("li").siblings().each(function(){
+					$(this).find("a").removeClass("selected");
 				});
+			});
+			
+			// 初始化 类别信息
+			$http({	method:'post',
+				url:"category/list?type=tree"
+			}).success(function(result){
+				$scope.categorys = angular.fromJson(result.data);
+				$scope.category = $scope.categorys[0];
+			}).error(function(){
+				alert("网络连接失败");
 			});
 		}
 	}];
--- src/com/app/controller/TopicController.java
+++ src/com/app/controller/TopicController.java
@@ -4,21 +4,22 @@ import com.app.model.Topic;
 import com.app.model.User;
 import com.app.util.AjaxResult;
 import com.app.util.DateUtils;
+import com.jfinal.kit.StrKit;
 
 public class TopicController extends BaseController{
 	
 	public void addOrUpdate() throws Exception{
-		AjaxResult result = new AjaxResult(1,"注册成功");
+		AjaxResult result = new AjaxResult(1,"操作成功");
 		try {
-			User user = this.getCurrUser();
-			if(user == null){
-				result.setMsg(0, "请重新登录");
+			Topic topic = this.getModel(Topic.class).setAttrs(this.getParamMap());
+			if(StrKit.notNull(topic.get("id"))){
+				topic.update();
 			}else{
-				Topic topic = this.getModel(Topic.class).setAttrs(this.getParamMap());
 				topic.set("create_time", DateUtils.getCurrDate());
-				topic.set("user_id", user.get("id"));
+				//topic.set("user_id", this.getCurrUser().get("id"));
 				topic.save();
 			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			result.setMsg(0, "提交失败："+e.getMessage());
