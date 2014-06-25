c164187432ed9a7b4f9a63903b3a5d6d627f7289
hewep <hewephao@163.com>
2014-06-25 20:05:34 +0800

    ...

--- src/test/Client.java
+++ src/test/Client.java
@@ -4,9 +4,16 @@ package test;
 import org.junit.Test;
 
 public class Client {
-	
+	enum Type{
+		RED("red"),BULE("");
+		
+		Type(String value){
+			
+		}
+	}
 	@Test
 	public void testDate(){
 			//System.out.println(DateUtils.g);
+		System.out.println(Type.BULE);
 	}
 }
