d5c9b35abcf0db41babae9aeda72dfdf32842c06
hewep <hewephao@163.com>
2014-06-29 23:11:11 +0800

    ...

---
+++ src/com/app/common/SessionListener.java
@@ -0,0 +1,20 @@
+package com.app.common;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class SessionListener implements HttpSessionListener{
+
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
