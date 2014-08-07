93423bad03f7af3c36b141cb185d9c77c6633942
hewep <hewephao@163.com>
2014-08-07 18:57:40 +0800

    基Model

---
+++ src/com/app/model/BaseModel.java
@@ -0,0 +1,10 @@
+package com.app.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class BaseModel<M extends Model<M>> extends Model<M>{
+
+	private static final long serialVersionUID = -4541089188014674024L;
+	
+	
+}
