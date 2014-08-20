e4689c194a551dbba252aac0d80699ba20ff4217
hewep <hewephao@163.com>
2014-08-20 19:08:12 +0800

    添加icon页面

--- WebRoot/res/admin/common/directives.js
+++ WebRoot/res/admin/common/directives.js
@@ -17,5 +17,19 @@ define(['angular'], function(){
 	        });
 		};
 	});
-	
+	// icon
+	comm.directive('icons', function(){
+		return function(scope, element, attr, ctrl){
+			$(element).find('li').css('cursor','pointer').on('click', function(){
+	            var ico_title = $(this).attr('title');
+	            var ico_class = $(this).find('i').attr('class');
+	            
+	            var type = attr.icons;
+	            if(type == 'class'){
+	            	ico_title = ico_class;
+	            }
+	            $(element).prev().find('span').html('[ <i class="'+ico_class+'"></i> <code>&lt;i class="'+ico_title+'"&gt;&lt;/i&gt;</code> ]');
+	        });
+		};
+	});
 });
